6f764914b0b03f13b110108890f8ed5739be6127
Rikyyyyyyyyyy <wenwen@ualberta.ca>
2022-04-26 12:00:16 -0600

    update to amazon db

--- Tproject/settings.py
+++ Tproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Tproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pycrfree',
+        'USER':'wenwen',
+        'PASSWORD':'Li704647903',
+        'HOST':'pycrfree.cvhutn0ofsx8.us-east-1.rds.amazonaws.com',
+        'PORT':'5432',
     }
 }
 
