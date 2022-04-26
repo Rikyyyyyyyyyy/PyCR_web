88877fcf39f7ceb8293d864cf6a5b7629784daab
Rikyyyyyyyyyy <wenwen@ualberta.ca>
2022-04-26 10:21:58 -0600

    update

--- Tproject/settings.py
+++ Tproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '75f44!sc#c*9h1p4ad_jm5)id%$8@31n7x69#vet@5a(a$spqv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
