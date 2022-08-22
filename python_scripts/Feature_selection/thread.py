4a89d0eaea5e4aee545af20529c5c46eedbf6479
Rikyyyyyyyyyy <wenwen@ualberta.ca>
2022-08-22 15:08:22 -0600

    update

--- python_scripts/Feature_selection/thread.py
+++ python_scripts/Feature_selection/thread.py
@@ -34,7 +34,7 @@ class PyCRThread(threading.Thread):
     def run(self):
         # try:
             s3 = boto3.resource('s3')
-            BUCKET = "pycr-wwb"
+            BUCKET = "pycr"
             PyCR.runPyCR(self.isExternal, self.splitratio, self.rocType, self.tupaType, self.isMotabo, self.motabo_url, self.sample_url, self.class_url, self.sampleName, self.VariableName, self.scaleType, self.iterations, self.survivalRate, self.rankingAlg, self.nComp, self.pk)
             shutil.make_archive('static/images/featureSelection/temp/zipOutput/output' + str(self.task.pk), "zip",
                                 'static/images/featureSelection/temp/output/', 'output' + str(self.task.pk))
